997b976f5a33fdb2674d10354521d106da44c52c
张森 <zhangsen@shiyedata.com>
2022-01-06 15:11:04 +0800

    更改title

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   publicPath: './',
   outputDir: 'docs',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '张森个人简历'
+      return args
+    })
+  },
   devServer: {
     open: true
   }
