8223c07bb19af3d0c6be1506226b4298bfa8e6ac
张森 <zhangsen@shiyedata.com>
2022-01-06 10:06:14 +0800

    eslint初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,16 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true, // 结尾加分号
+        singleQuote: true, // 使用单引号
+        'prettier.printWidth': 500, // 超过最大值换行
+        tabWidth: 2, // 缩进字节数
+        'trailingComma': 'none', // 对象末尾不加逗号
+        'arrowParens': 'avoid'
+      }
+    ]
   },
 };
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath: './',
+  assetsDir: 'docs',
+  devServer: {
+    open: true
+  }
+};
